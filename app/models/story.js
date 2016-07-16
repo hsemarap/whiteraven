ddbfbf18adeb06acb454e71534e15439e2364169
Parameswaran D <desigavi@adobe.com>
2016-07-17 02:06:14 +0530

    Tweet API endpoint added

--- app/models/story.js
+++ app/models/story.js
@@ -1,14 +1,16 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var findOrCreate = require('mongoose-findorcreate');
+
 var StorySchema = new Schema({
     url: {type: String, default: ''},
     title: {type: String, default: ''},
     description: {type: String, default: ''},
-    tags: [{type: Array, default: []}],
+    tags: [{type: String, default: ''}],
     addedBy: [{type: String, default: ''}],
     createdAt: {type: Date, default: Date.now},
     updatedAt: {type: Date, default: Date.now},
+    tweet_id: {type: String, default: ''},
 });
 StorySchema.plugin(findOrCreate);
 module.exports = mongoose.model('Story', StorySchema);
